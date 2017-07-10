69e51f2e4cd222520ddc6e02e83bbd49492bbc8e
DESKTOP-0G7OST1\Bloody <Bloody1410@web.de>
2017-07-10 13:17:59 +0200

    jquary fixxed

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,9 +1,32 @@
-// JavaScript
+// JavaScript Document
 
-  $(document).ready(funktion(e){
-      $('.lw_icon').hover(function(e){
-        $(this).children('span').stop().animate({left:'0px'},200);
+$(document).ready(function(e) {
+
+	$(window).scroll(function(e) {
+
+		var document_height = $(document).height();
+		var scroll_position = $(window).scrollTop();
+
+		var scroll_action = ((scroll_position)/(document_height-$(window).height()))*100;
+
+		if(scroll_position > 850) {
+
+			$('#ud_gesamt_scrollbar').stop().animate({top: '65px'},300);
+
+		};
+
+		if(scroll_position < 850) {
+
+			$('#ud_gesamt_scrollbar').stop().animate({top: '0px'},300);
+
+		};
+
+		$('#ud_active_scrollbar').stop().animate({width: scroll_action + '%'},200);
+
+	});
+  $('.lw_icon').hover(function(e){
+        $(this).children('span').stop().animate({right:'0px'},200);
       },function(e) {
-        $(this).children('span').stop().animate({left:'-70px'},200);
+        $(this).children('span').stop().animate({right:'-70px'},200);
       });
-)};
+});
